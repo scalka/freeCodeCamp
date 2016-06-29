60b687681e1a0f528ea2a9dd6a0f554e8cbf774e
scalka <calka.sylwia@gmail.com>
2016-06-29 10:00:20 +0200

    four more exercises

--- algorith_scripting.js
+++ algorith_scripting.js
@@ -24,3 +24,60 @@ function findLongestWord(str) {
   });  
   return myArray[0].length;
 }
+// TITLE CASE A SENTENCE
+function titleCase(str) {
+  var myArray = [];
+  str = str.toLowerCase();
+  myArray = str.split(" ");
+  
+ for (var i = 0; i < myArray.length; i++){
+   myArray[i] = myArray[i].replace(myArray[i].charAt(0), function toUpper(x){
+     return x.toUpperCase();}); 
+ }
+  return myArray.join(' ');
+}
+// RETURN LARGEST NUMBET IN ARRAY
+function largestOfFour(arr) {
+var max = [];
+  for (var i=0; i<arr.length; i++){
+    var largest =0;
+    for(var j=0; j<arr[i].length; j++){
+        if (arr[i][j] > largest){
+        largest = arr[i][j];
+      }      
+    }
+    max[i] = largest;
+  }
+  return max;
+}
+//CONFIRM THE ENDING
+function confirmEnding(str, target) {
+  if(str.substring(str.length-1) === target){
+    return true;
+  } else if ( str.substr(str.length-target.length) === target ){
+    return true;
+  } else {
+    return false;
+  }
+}
+// REPEAT A STRING
+function repeatStringNumTimes(str, num) {
+  var string;
+  if (num > 0){
+    string = str.repeat(num) ;
+  } else {
+    string = "";
+  }
+  return string;
+}
+//TRUNCATE A STRING
+function truncateString(str, num) {
+  if (str.length > num){
+    if (num <= 3) {
+      str = str.slice(0, num)+"...";
+    } else {
+      str = str.slice(0, num-3)+"...";
+    }      
+  } 
+  return str;
+}
